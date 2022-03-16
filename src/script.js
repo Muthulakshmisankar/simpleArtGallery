7c8323a39eaa24302675763ba676d1e565f2c95b
Muthulakshmi <muthulakshmi.s@accubits.com>
2022-03-16 10:06:56 +0530

    threed model loaded

--- src/script.js
+++ src/script.js
@@ -2,30 +2,61 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 /**
  * Base
  */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+/**scene */
+const scene = new THREE.Scene( )
+// scene.background = null
 /**
  * GlTF loader
  */
 const url = 'DamagedHelmet/glTF/DamagedHelmet.gltf'
-
+const url1 = 'the-great-drawing-room/source/src/model.obj'
+const url2 = ''
 const loader = new GLTFLoader();
-loader.load(url, ((gltf) => {
+loader.load(url2, ((gltf) => {
     // gltf.scene.scale.set(300, 300, 300);
     // gltf.scene.position.set(220, 10, 400)
-    scene.add(gltf.scene.children[0]);
-// console.log(gltf)
+    // scene.add(gltf.scene.children[0]);
+console.log(gltf)
     // camera.position.z = 7
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 }), undefined, ((err) => { console.log(err) }))
 
+const objloader = new OBJLoader();
 
+// load a resource
+objloader.load(
+	// resource URL
+	 url1,
+	// called when resource is loaded
+	function ( object ) {
+        console.log(object)
+        let mesh = object.children[0]
+        mesh.position.y = -2
+		scene.add(mesh  );
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
 
 /**
  * Sizes
@@ -58,7 +89,7 @@ camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
 
-// scene.add(camera)
+scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -68,10 +99,37 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    //  alpha: true,
+    //  antialias: true 
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Lights
+ */
+ const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
+ scene.add(ambientLight)
+ 
+ const directionalLight = new THREE.DirectionalLight(0xff0000, 0.5)
+ directionalLight.castShadow = true
+ directionalLight.shadow.mapSize.set(1024, 1024)
+ directionalLight.shadow.camera.far = 15
+ directionalLight.shadow.camera.left = - 7
+ directionalLight.shadow.camera.top = 7
+ directionalLight.shadow.camera.right = 7
+ directionalLight.shadow.camera.bottom = - 7
+ directionalLight.position.set(- 5, 5, 0)
+//  scene.add(directionalLight)
+
+const pointLight = new THREE.PointLight(0xffffff,0.8)
+pointLight.position.set(4,4,4)
+const pointLight2 = new THREE.PointLight(0xff0000,0.8)
+pointLight2.position.set(0,0,0)
+const pointLight3 = new THREE.PointLight(0x0000ff,0.8)
+pointLight3.position.set(0,-2,0)
+scene.add(pointLight , pointLight2 )
 
 /**
  * Animate
