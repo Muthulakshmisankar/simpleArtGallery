c3b9ccf8c87ab379fbc30941e0aef545c161993d
Muthulakshmi <muthulakshmi.s@accubits.com>
2022-03-19 19:54:23 +0530

    keyboard event

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+// import * as THREEx from 'threex';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
@@ -10,8 +11,10 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 const canvas = document.querySelector('canvas.webgl')
 
 /**scene */
-const scene = new THREE.Scene( )
+const scene = new THREE.Scene()
 // scene.background = null
+
+// const keyboard = new THREEx.KeyboardState();
 /**
  * GlTF loader
  */
@@ -23,7 +26,7 @@ loader.load(url2, ((gltf) => {
     // gltf.scene.scale.set(300, 300, 300);
     // gltf.scene.position.set(220, 10, 400)
     // scene.add(gltf.scene.children[0]);
-console.log(gltf)
+    console.log(gltf)
     // camera.position.z = 7
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -34,28 +37,29 @@ const objloader = new OBJLoader();
 
 // load a resource
 objloader.load(
-	// resource URL
-	 url1,
-	// called when resource is loaded
-	function ( object ) {
+    // resource URL
+    url1,
+    // called when resource is loaded
+    function (object) {
         console.log(object)
         let mesh = object.children[0]
+        mesh.name = 'goldenhouse'
         mesh.position.y = -2
-		scene.add(mesh  );
+        scene.add(mesh);
 
-	},
-	// called when loading is in progresses
-	function ( xhr ) {
+    },
+    // called when loading is in progresses
+    function (xhr) {
 
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
 
-	},
-	// called when loading has errors
-	function ( error ) {
+    },
+    // called when loading has errors
+    function (error) {
 
-		console.log( 'An error happened' );
+        console.log('An error happened');
 
-	}
+    }
 );
 
 /**
@@ -94,7 +98,12 @@ scene.add(camera)
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-
+controls.keys = {
+    LEFT: 'ArrowLeft', //left arrow
+    UP: 'ArrowUp', // up arrow
+    RIGHT: 'ArrowRight', // right arrow
+    BOTTOM: 'ArrowDown' // down arrow
+}
 /**
  * Renderer
  */
@@ -109,39 +118,39 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Lights
  */
- const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
 //  scene.add(ambientLight)
- const ambientLight2 = new THREE.AmbientLight(0x2a622a, 0.8)
+const ambientLight2 = new THREE.AmbientLight(0x2a622a, 0.8)
 //  scene.add(ambientLight2)
- const directionalLight = new THREE.DirectionalLight(0xff0000, 0.5)
- directionalLight.castShadow = true
- directionalLight.shadow.mapSize.set(1024, 1024)
- directionalLight.shadow.camera.far = 15
- directionalLight.shadow.camera.left = - 7
- directionalLight.shadow.camera.top = 7
- directionalLight.shadow.camera.right = 7
- directionalLight.shadow.camera.bottom = - 7
- directionalLight.position.set(- 5, 5, 0)
+const directionalLight = new THREE.DirectionalLight(0xff0000, 0.5)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(- 5, 5, 0)
 //  scene.add(directionalLight)
 
-const pointLight = new THREE.PointLight(0xCC7722,0.4)
+const pointLight = new THREE.PointLight(0xCC7722, 0.4)
 // pointLight.position.set(4,4,4)
-const pointLight2 = new THREE.PointLight(0xff0000,0.3)
-pointLight2.position.set(-6,-6,0)
-const pointLight3 = new THREE.PointLight(0xFFD800,0.4)
-pointLight3.position.set(-4,-4,0)
+const pointLight2 = new THREE.PointLight(0xff0000, 0.3)
+pointLight2.position.set(-6, -6, 0)
+const pointLight3 = new THREE.PointLight(0xFFD800, 0.4)
+pointLight3.position.set(-4, -4, 0)
 
-const floorLight = new THREE.PointLight(0xff0000,0.4)
-floorLight.position.set(0,0,0)
-const floorLight1 = new THREE.PointLight(0xffffff,0.4)
-floorLight1.position.set(-4,0,0)
-const floorLight2 = new THREE.PointLight(0xffffff,0.4)
-floorLight.position.set(4,0,0)
-const floorLight3 = new THREE.PointLight(0xffffff,0.4)
-floorLight3.position.set(8,0,0)
-const greenLight = new THREE.PointLight(0x37a52e,0.4)
+const floorLight = new THREE.PointLight(0xff0000, 0.4)
+floorLight.position.set(0, 0, 0)
+const floorLight1 = new THREE.PointLight(0xffffff, 0.4)
+floorLight1.position.set(-4, 0, 0)
+const floorLight2 = new THREE.PointLight(0xffffff, 0.4)
+floorLight.position.set(4, 0, 0)
+const floorLight3 = new THREE.PointLight(0xffffff, 0.4)
+floorLight3.position.set(8, 0, 0)
+const greenLight = new THREE.PointLight(0x37a52e, 0.4)
 // greenLight.position.set(4,-4,0)
-scene.add(pointLight ,pointLight2 , pointLight3 , floorLight , floorLight2)
+scene.add(pointLight, pointLight2, pointLight3, floorLight, floorLight2)
 
 /**
  * Animate
@@ -154,12 +163,50 @@ const tick = () => {
     controls.update()
     // Render
     renderer.render(scene, camera)
+    // var delta = clock.getDelta(); // seconds.
+    // let moveDistance = 200 * elapsedTime; // 200 pixels per second
+    // let rotateAngle = Math.PI / 2 * elapsedTime;   // pi/2 radians (90 degrees) per second
+
+    // let HouseMesh = scene.getObjectByName('goldenhouse');
+    // if (keyboard.pressed("A"))
+    //     HouseMesh.rotation.y += rotateAngle;
+    // if (keyboard.pressed("D"))
+    //     HouseMesh.rotation.y -= rotateAngle;
+
+    // if (keyboard.pressed("left"))
+    //     HouseMesh.position.x -= moveDistance;
+    // if (keyboard.pressed("right"))
+    //     HouseMesh.position.x += moveDistance;
+    // if (keyboard.pressed("up"))
+    //     HouseMesh.position.z -= moveDistance;
+    // if (keyboard.pressed("down"))
+    //     HouseMesh.position.z += moveDistance;
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
-
-
+const setupKeyControls = () => {
+   
+    
+    document.onkeydown = (e) => {
+        const HouseMesh = scene.getObjectByName('goldenhouse');
+        switch (e.key) {
+            case "a":
+                HouseMesh.position.x += 0.1;
+                break;
+            case "w":
+                HouseMesh.position.z -= 0.1;
+                break;
+            case "s":
+                HouseMesh.position.x -= 0.1;
+                break;
+            case "d":
+                HouseMesh.position.z += 0.1;
+                break;
+        }
+    };
+}
+setupKeyControls()
 
 tick()
